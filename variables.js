10c7a11dd6f1ed8b27c4c56bd1af18e91e155522
Kigo-P <peterkigo33@gmail.com>
2024-06-19 12:15:45 +0300

    JS Variables

---
+++ variables.js
@@ -0,0 +1,68 @@
+//Part 1: Declaring and Initializing Variables
+//Declare a variable age and initialize it with your age.
+
+let age = 27;
+console.log(age);
+
+//Declare a variable name and initialize it with your name.
+
+let name = "Peter";
+console.log(name);
+
+//Declare a variable isStudent and initialize it with a boolean value indicating if you are a student or not.
+
+let isStudent = true;
+console.log(isStudent);
+
+//Part 2: Performing Operations
+//Declare a variable x and initialize it with the value 10.
+
+let x = 10;
+console.log(x);
+
+//Declare a variable y and initialize it with the value 5.
+
+let y = 5;
+console.log(y);
+
+//Declare a variable sum and assign it the result of adding x and y.
+
+let sum = x + y;
+console.log(sum);
+
+//Declare a variable product and assign it the result of multiplying x and y.
+
+let product = x * y;
+console.log(product);
+
+//Declare a variable difference and assign it the result of subtracting y from x.
+
+let difference = x - y;
+console.log(difference);
+
+//Declare a variable quotient and assign it the result of dividing x by y.
+
+let quotient = x/y;
+console.log(quotient);
+
+//Get the remainder between x and y;
+
+let remainder  = x%y;
+console.log(remainder);
+
+//Part 3: Working with Strings
+//Declare a variable greeting and assign it a string that says "Hello, my name is " followed by your name.
+
+let greeting = "Hello, my name is " + name;
+console.log(greeting);
+
+//Declare a variable fullGreeting and use template literals to create a string that includes your name and age.
+
+const fullGreeting = greeting + " " + age;
+console.log(fullGreeting);
+
+//Part 4: Outputting Results
+//Print each of the variables from Parts 1, 2, and 3 to the console.
+
+
+
